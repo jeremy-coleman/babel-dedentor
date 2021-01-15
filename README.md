3b29c3af1c730e2fb32d6a48f96efa32e94f9e32
proteriax <8125011+proteriax@users.noreply.github.com>
2021-01-15 02:55:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# babel-dedenter
+
+[https://github.com/MartinKolarik/babel-plugin-dedent](babel-plugin-dedent) with more configuration.
+
+## Install
+
+```sh
+yarn add --save-dev babel-dedenter
+```
+
+In your `babel.config.json`:
+
+```json
+{
+  "plugins": [
+    [
+      "babel-dedenter",
+      {
+        // Showing default configuration, all of the following fields are optional
+        "tagName": "dedent",
+        // Retain the function call, useful for dedenting SQL statements and `chalk` strings.
+        "keepFunctionCall": false,
+        "trimLeft": true,
+        "trimRight": true
+      }
+    ]
+  ]
+}
+```
+
+Prefer type safe Babel configuration? Try the following:
+
+```js
+import { useDedentPlugin } from "babel-dedenter"
+
+export default {
+  plugins: [
+    useDedentPlugin({
+      tagName: "dedent",
+      keepFunctionCall: false,
+      trimLeft: true,
+      trimRight: true,
+    }),
+  ],
+}
+```
+
+## Usage
+
+Indentation will be removed from all strings tagged with `dedent` tag (or any tag specified in your configuration).
+
+```js
+expect(dedent`Line #1
+	Line #2
+	Line #3`).to.equal("Line #1\nLine #2\nLine #3")
+
+// Leading/trailing line break is removed.
+expect(
+  dedent`
+	Line #1
+	Line #2
+	Line #3
+	`
+).to.equal("Line #1\nLine #2\nLine #3")
+
+// No more than one leading/trailing line break is removed.
+expect(
+  dedent`
+
+	Line #1
+	Line #2
+	Line #3
+
+	`
+).to.equal("\nLine #1\nLine #2\nLine #3\n")
+
+// Only the "base" indentation is removed.
+expect(
+  dedent`
+	Line #1
+		Line #2
+			Line #3
+	`
+).to.equal("Line #1\n\tLine #2\n\t\tLine #3")
+
+// The last line is ignored if it doesn't contain anything else than whitespace.
+expect(
+  (function () {
+    return dedent`
+			Line #1
+			Line #2
+			Line #3
+		`
+  })()
+).to.equal("Line #1\nLine #2\nLine #3")
+
+// Escaped characters are ignored.
+expect(
+  dedent`
+	\tLine #1
+	\tLine #2
+	\tLine #3
+	`
+).to.equal("\tLine #1\n\tLine #2\n\tLine #3")
+```
+
+## License
+
+Copyright (c) 2020 ~proteria.
+Copyright (c) 2015 - 2020 Martin Kolárik.
+
+Released under the MIT license.
---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import type { PluginObj, PluginItem, PluginPass, PluginOptions } from "@babel/core"
+import type { TemplateElement } from "@babel/types"
+import invariant from "tiny-invariant"
+import { last } from "lodash"
+
+type DedentPluginOptions = {
+  /** Default is `dedent` */
+  tagName: string
+  /** Default is false */
+  keepFunctionCall: boolean
+  /** Default is true */
+  trimLeft?: boolean
+  /** Default is true */
+  trimRight?: boolean
+}
+
+export function useDedentPlugin(options: DedentPluginOptions): PluginItem {
+  return [pluginDedent, options]
+}
+
+export default function pluginDedent(babel: {
+  types: typeof import("@babel/types")
+}): PluginObj<PluginPass & { opts: PluginOptions & DedentPluginOptions }> {
+  const t = babel.types
+
+  return {
+    visitor: {
+      CallExpression(path, { opts }) {
+        const { node } = path
+
+        if (t.isIdentifier(node.callee, { name: opts.tagName ?? "dedent" })) {
+          const [arg] = node.arguments
+          if (t.isTemplateLiteral(arg)) {
+            transform(arg.quasis, opts)
+            if (!opts.keepFunctionCall) path.replaceWith(arg)
+          } else if (t.isTaggedTemplateExpression(arg)) {
+            transform(arg.quasi.quasis, opts)
+            if (!opts.keepFunctionCall) path.replaceWith(arg)
+          }
+        }
+      },
+      TaggedTemplateExpression(path, { opts }) {
+        const { node } = path
+
+        if (t.isIdentifier(node.tag, { name: opts.tagName ?? "dedent" })) {
+          transform(node.quasi.quasis, opts)
+          if (!opts.keepFunctionCall) path.replaceWith(node.quasi)
+        }
+      },
+    },
+  }
+}
+
+function replaceQuasi(
+  value: TemplateElement["value"],
+  pattern: RegExp,
+  replacement: string
+) {
+  value.raw = value.raw.replace(pattern, replacement)
+  value.cooked = value.cooked?.replace(pattern, replacement)
+}
+
+function transform(quasis: TemplateElement[], opts: DedentPluginOptions) {
+  invariant(quasis.every(e => e.type === "TemplateElement"))
+
+  if (opts.trimRight ?? true) {
+    trimRight(last(quasis)!)
+  }
+
+  const matches = quasis
+    .map(q => q.value.raw.match(/\n[\t ]+/g))
+    .filter((x): x is NonNullable<typeof x> => x != null)
+    .flat(1)
+
+  if (matches.length) {
+    const size = Math.min(...matches.map(value => value.length - 1))
+    const pattern = RegExp(`\n[\t ]{${size}}`, "g")
+
+    for (const { value } of quasis) {
+      replaceQuasi(value, pattern, "\n")
+    }
+  }
+
+  if (opts.trimLeft ?? true) {
+    leftTrim(quasis[0])
+  }
+}
+
+function leftTrim({ value }: TemplateElement) {
+  const pattern = /^\r?\n/
+  if (pattern.test(value.raw)) {
+    replaceQuasi(value, pattern, "")
+  }
+}
+
+function trimRight({ value }: TemplateElement) {
+  const match = value.raw.match(/\r?\n([\t ]*)$/)?.[1]
+  if (match) {
+    replaceQuasi(value, RegExp(`\r?\n[\t ]{${match.length}}$`), "")
+  }
+}
